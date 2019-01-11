64d0865e767c651a99ee809e38ac31e5d0ca9de1
Francis Dutil <francis.dutil@imagia.com>
2019-01-11 20:06:36 +0000

    Removing useless command in the .sh

--- install.sh
+++ install.sh
@@ -3,9 +3,6 @@
 
 python -m venv .env
 . .env/bin/activate # Need to run this everytime we want to run the project. 
-sudo $(which pip) install -e .
-sudo $(which pip) install -r requirements.txt
-
-export LC_ALL=C.UTF-8 
-export LANG=C.UTF-8
+sudo pip install -e .
+sudo pip install -r requirements.txt
 
