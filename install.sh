34a50295b1e3be974f6748df47abef5fc40f47a6
TrofimovAssya <petitkalimero@gmail.com>
2019-08-06 12:53:55 -0400

    Revert "Removing useless command in the .sh"
    This reverts commit 64d0865e767c651a99ee809e38ac31e5d0ca9de1.

--- install.sh
+++ install.sh
@@ -3,6 +3,9 @@
 
 python -m venv .env
 . .env/bin/activate # Need to run this everytime we want to run the project. 
-sudo pip install -e .
-sudo pip install -r requirements.txt
+sudo $(which pip) install -e .
+sudo $(which pip) install -r requirements.txt
+
+export LC_ALL=C.UTF-8 
+export LANG=C.UTF-8
 
